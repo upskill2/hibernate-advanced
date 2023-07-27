e54407d69a1a18636e13c7a27b6d289d213ce705
taras.chmeruk <taras.chmeruk@gameloft.com>
2023-07-27 16:24:45 +0300

    s03

---
+++ s03-collections-map/src/main/java.hibernate/CreateStudentImagesMapDemo.java
@@ -0,0 +1,62 @@
+import entity.Student;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+public class CreateStudentImagesMapDemo {
+    public static void main (String[] args)  {
+        SessionFactory sessionFactory =  new Configuration ()
+                .configure ("hibernate.cfg.xml")
+                .addAnnotatedClass (Student.class)
+                .buildSessionFactory ();
+
+        Session session = sessionFactory.getCurrentSession ();
+
+        try {
+            Student student1 = new Student ("John", "Doe", "John@gmail.com");
+
+            Set<String> theImages = student1.getImages ();
+            theImages.add ("photo1.jpg");
+            theImages.add ("photo2.jpg");
+            theImages.add ("photo3.jpg");
+            theImages.add ("photo4.jpg");
+            theImages.add ("photo4.jpg");
+
+            List<String> listImages1 = student1.getImagesList ();
+            listImages1.add ("photoA.jpg");
+            listImages1.add ("photoB.jpg");
+            listImages1.add ("photoC.jpg");
+            listImages1.add ("photoC.jpg");
+
+
+
+            Student student2 = new Student ("Mary", "Public", "Mary@com");
+            Student student3 = new Student ("Bonita", "Applebum", "Bonita@com");
+            Student student4 = new Student ("James", "Bond", "James@com");
+
+
+            session.beginTransaction ();
+            log.info ("Saving the students and images");
+            session.persist (student1);
+            session.persist (student2);
+            session.persist (student3);
+            session.persist (student4);
+
+            session.getTransaction ().commit ();
+            log.info ("Done!");
+
+        } catch (Exception e) {
+            log.error ("SQLException: " + e.getMessage());
+
+        } finally {
+            session.close ();
+            sessionFactory.close ();
+        }
+    }
+}
---
+++ s03-collections-map/src/main/java.hibernate/entity/Student.java
@@ -0,0 +1,50 @@
+package entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table (name = "student")
+@Data
+public class Student {
+
+    @Id
+    @GeneratedValue (strategy = javax.persistence.GenerationType.IDENTITY)
+    private int id;
+
+    @Column (name = "first_name")
+    private String firstName;
+
+    @Column (name = "last_name")
+    private String lastName;
+
+    @Column (name = "email")
+    private String email;
+
+
+    @ElementCollection
+    @CollectionTable (name = "image", joinColumns = @JoinColumn (name = "student_id"))
+    @Column (name = "file_name")
+    private Set<String> images = new HashSet<> ();
+
+    @ElementCollection
+    @CollectionTable (name = "images_list", joinColumns = @JoinColumn (name = "student_id"))
+    @Column (name = "file_name_list")
+    @OrderColumn
+    private List<String> imagesList = new ArrayList<> ();
+
+
+    @ElementCollection
+    @CollectionTable (name = "images_map", joinColumns = @JoinColumn (name = "student_id"))
+    @Column (name = "image_name")
+    @MapKeyColumn (name = "file_name")
+    Map<String, String> imagesMap = new HashMap<> ();
+
+    public Student (String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
